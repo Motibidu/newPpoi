4d07a8252ce4e34eda19ff6a5ac03c4bbc963d9e
Motibidu <jack981109@gmail.com>
2021-04-26 23:12:53 +0900

    complete simple replacing text

--- newpoi/src/main/java/org/dstadler/poiandroidtest/newpoi/expanded_screen.java
+++ newpoi/src/main/java/org/dstadler/poiandroidtest/newpoi/expanded_screen.java
@@ -1,8 +1,14 @@
 package org.dstadler.poiandroidtest.newpoi;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Point;
 import android.net.Uri;
@@ -10,6 +16,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
@@ -23,41 +30,55 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class expanded_screen extends AppCompatActivity {
 
     private ImageButton expanded_screen_backButton, plus;
     private ImageView expanded_screen_mainImageView;
-    private String folder, fileName;
+    private String folder, fileName, filePath;
     private FirebaseAuth mAuth;
     private FirebaseStorage fStorage;
     private StorageReference storageReference;
     private Uri fetchUri;
     public Uri return_retrievedURI;
+    private static final int MY_PERMISSION_STORAGE = 1111;
 
     public interface MyCallback {
         void onCallback(Uri value);
@@ -97,156 +118,121 @@ public class expanded_screen extends AppCompatActivity {
         plus.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
-//                File targetFile = new File(retrievedURI.getPath());
-//                try{
-//                    XWPFDocument doc = new XWPFDocument(OPCPackage.open(targetFile));
-//                }catch(InvalidFormatException | IOException e){
-//                    Log.e("expanded_screen", e.toString());
-//                }
-//                try {
-//                    XWPFDocument doc = new XWPFDocument(OPCPackage.open(targetFile));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } catch (InvalidFormatException e) {
-//                    e.printStackTrace();
-//                }
-
-//                try{
-//                    createDOCX();
-//
-//                } catch (FileNotFoundException e){
-//                    e.printStackTrace();
-//                }
-            }
-        });
-
-
-//        if (getSoftButtonsBarHeight() == 0) {
-//            height = size.y - getStatusBarHeight() - getSoftButtonsBarHeight() - (int) dipToPixels(expanded_screen.this, 104);
-//
-//        } else {
-//            height = size.y - getStatusBarHeight() - getSoftButtonsBarHeight() - (int) dipToPixels(expanded_screen.this, 56);
-//        }
-//        ViewTreeObserver observer = expanded_screen_backButton.getViewTreeObserver();
-//        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-//
-//            @Override
-//            public void onGlobalLayout() {
-//                lay_height = expanded_screen_backButton.getHeight();
-//                int headerLayoutWidth = expanded_screen_backButton.getWidth();
-//                expanded_screen_backButton.getViewTreeObserver().removeOnGlobalLayoutListener(
-//                        this);
-//
-//                Log.v("height", lay_height + "");
-//
-//            }
-//        });
-
-
-//        imageView2.setImageURI(null);
-//        imageView2.setImageURI(imgUri);
-//
-//        imageButton = (ImageButton)findViewById(R.id.back_button);
-//        imageButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                onBackPressed();
-//            }
-//        });
-//        bottomNavigationView = findViewById(R.id.expaned_screen_bottom_navigation);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch(item.getItemId()){
-//                    case R.id.create_action:
-//                        break;
-//                }
-//                return true;
-//            }
-//        });
-//        scroll_main = findViewById(R.id.scroll_main);
-//        plus = findViewById(R.id.plus);
-//        scroll_main.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-//            @Override
-//            public void onScrollChanged() {
-//                new Handler().post(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        int scrollX = scroll_main.getScrollX(); //for horizontalScrollView
-//                        int scrollY = scroll_main.getScrollY(); //for verticalScrollView
-//
-//
-//                        int sc = scrollY + height;
-//
-//                        Log.v("bottom", lay_height + "  Y=" + sc + "  " + scrollY + "   " + height);
-//
-//                        if (sc >= lay_height) {
-//                            plus.setVisibility(View.GONE);
-//
-//                        } else {
-//                            plus.setVisibility(View.VISIBLE);
-//                        }
-//                    }
-//                });
-//            }
-//        });
-//        ll = (ScrollView)findViewById(R.id.ll);
-//        ll.setOnTouchListener(new View.OnTouchListener() {
-//            @Override public boolean onTouch(View v, MotionEvent event)
-//            {
-//                ll.requestDisallowInterceptTouchEvent(true);
-//                return false;
-//            } });
-    }
-
-    private void createDOCX() throws FileNotFoundException {
-
-        XWPFDocument doc = new XWPFDocument();
-        XWPFParagraph tmpParagraph = doc.createParagraph();
-        XWPFRun tmpRun = tmpParagraph.createRun();
-        tmpRun.setText("DDDDDDDDDDDDDDDDDDDDDD");
-
-        folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
-        fileName = "/newFile.docx";
-        File f = new File(folder);
-
-        if(!f.exists()) {
-            Log.e("AAA","DDD");
-        }
-        FileOutputStream out = new FileOutputStream(new File(folder+fileName));
-        try{
-            doc.write(out);
-            doc.close();
-//            Toast.makeText(expanded_screen.this,"CREATED",Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void retrieve_DOCX_URI(MyCallback myCallback){
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-
-        StorageReference profileRef = storageReference.child("Documents/self_introduction2.doc");
-        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                File file = new File(uri.getPath());
+                retrieve_DOCX_URI();
+                checkPermission();
                 try {
-                    FileInputStream fis = new FileInputStream(file.getAbsolutePath());
-                    XWPFDocument document = new XWPFDocument(fis);
+                    InputStream is = new FileInputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/AP/self_introduction2.docx");
 
-                    List<XWPFParagraph> paragraphs = document.getParagraphs();
+                    XWPFDocument docx = new XWPFDocument(OPCPackage.open(is));
+                    for (XWPFTable tbl : docx.getTables()) {
+                        for (XWPFTableRow row : tbl.getRows()) {
+                            for (XWPFTableCell cell : row.getTableCells()) {
+                                for (XWPFParagraph p : cell.getParagraphs()) {
+                                    for (XWPFRun r : p.getRuns()) {
+                                        String text = r.getText(0);
+                                        if (text != null && text.contains("needle")) {
+                                            text = text.replace("needle", "haystack");
+                                            r.setText(text,0);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    final FileOutputStream out = new FileOutputStream(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/AP/edited_self_introduction2.docx"));
+                    docx.write(out);
+
+                    out.close();
+                    docx.close();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
+                } catch (InvalidFormatException e) {
+                    e.printStackTrace();
                 }
-
 
             }
         });
+
     }
+
+    private void retrieve_DOCX_URI(){
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+
+        StorageReference profileRef = storageReference.child("Documents/self_introduction2.docx");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+
+//                downloadFile(expanded_screen.this,"self_introduction2",".docx", folder, uri.toString());
+            }
+        });
+    }
+
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
+        DownloadManager downloadManager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setVisibleInDownloadsUi (true);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/AP/"+fileName+fileExtension);
+
+        downloadManager.enqueue(request);
+
+    }
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 다시 보지 않기 버튼을 만드려면 이 부분에 바로 요청을 하도록 하면 됨 (아래 else{..} 부분 제거)
+            // ActivityCompat.requestPermissions((Activity)mContext, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_CAMERA);
+
+            // 처음 호출시엔 if()안의 부분은 false로 리턴 됨 -> else{..}의 요청으로 넘어감
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("알림")
+                        .setMessage("저장소 권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
+                        .setNeutralButton("설정", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                intent.setData(Uri.parse("package:" + getPackageName()));
+                                startActivity(intent);
+                            }
+                        })
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_STORAGE);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_STORAGE:
+                for (int i = 0; i < grantResults.length; i++) {
+                    // grantResults[] : 허용된 권한은 0, 거부한 권한은 -1
+                    if (grantResults[i] < 0) {
+                        Toast.makeText(expanded_screen.this, "해당 권한을 활성화 하셔야 합니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                // 허용했다면 이 부분에서..
+
+                break;
+        }
+    }
+
+
 
 
 }
